42db0a36cf75e0e52ec13323599152bcf6c5ee2f
Gontier <alexis.gontier03@gmail.com>
2025-08-10 20:18:52 +0200

    ✨ Réactivation de la vérification de l'environnement dans TestPage et suppression du code de récupération des données par Prisma

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,22 +1,15 @@
-// import { env } from "@/utils/env"
-// import { notFound } from "next/navigation"
-import { prisma } from "@/lib/prisma"
+import { env } from "@/utils/env"
+import { notFound } from "next/navigation"
 
 export default async function TestPage() {
 
-    // if (env.NODE_ENV !== "development") {
-    //     notFound()
-    // }
-
-    const defaults = await prisma.default.findMany();
+    if (env.NODE_ENV !== "development") {
+        notFound()
+    }
 
     return (
         <div>
-            <pre>
-                <code>
-                    {JSON.stringify(defaults, null, 2)}
-                </code>
-            </pre>
+            Page test
         </div>
     )
 }
