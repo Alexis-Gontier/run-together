8489bc12fb9737e908333ec253fc2a333d4361b8
Gontier <alexis.gontier03@gmail.com>
2025-08-11 23:18:01 +0200

    💄 Ajout du logo de l'app

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,9 @@
 import { env } from '@/utils/env';
+import LOGO from '@/icon.svg';
 
 export const APP_URL =
   env.NODE_ENV === 'production'
     ? 'https://l6-runtogether.vercel.app/'
     : 'http://localhost:3000'
+
+export const APP_LOGO = LOGO
