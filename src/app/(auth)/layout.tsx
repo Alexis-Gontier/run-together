0af56e3a91941c265efe62fde9c5161b17728bd5
Gontier <alexis.gontier03@gmail.com>
2025-08-11 23:19:39 +0200

    💄 Amélioration de la mise en page de la page de connexion : ajout du logo, des en-têtes et d'un bouton "Mot de passe oublié ?"

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 export default function AuthLayout({ children } : { children: React.ReactNode }) {
     return (
-        <div className="max-w-sm mx-auto flex flex-col items-center justify-center min-h-screen">
+        <div className="max-w-md mx-auto px-6 flex flex-col items-center justify-center min-h-screen">
             {children}
         </div>
     );
--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -2,16 +2,49 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardHeader,
   CardTitle
 } from "@/components/shadcn-ui/card"
 import { SignInForm } from "./sign-in-form"
+import Image from "next/image"
+import { APP_LOGO } from "@/utils/constants"
+import { Separator } from "@/components/shadcn-ui/separator"
+import { Button } from "@/components/shadcn-ui/button"
+import Link from "next/link"
 
 export default function SignUpPage() {
   return (
-    <Card className="w-full">
-      <CardContent>
-        <SignInForm />
-      </CardContent>
-    </Card>
+    <div className="space-y-4 w-full">
+      <h1 className="flex items-center justify-center gap-2">
+        <Image
+          priority
+          src={APP_LOGO}
+          alt="logo"
+          width={36}
+          height={36}
+        />
+        <span className="font-medium">RunTogether.</span>
+      </h1>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle className="">Connexion</CardTitle>
+          <CardDescription>Connectez-vous à votre compte</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-6">
+          <Separator />
+          <SignInForm />
+          <Separator />
+          <Button
+            className="w-full cursor-pointer"
+            variant="outline"
+          >
+            Mot de passe oublié ?
+          </Button>
+        </CardContent>
+      </Card>
+      <div className="text-center text-xs px-6 text-muted-foreground">
+        En continuant, vous acceptez <Link href="" className="underline">nos Conditions d'utilisation</Link> et notre <Link href="" className="underline">Politique de confidentialité</Link>
+      </div>
+    </div>
   )
 }
