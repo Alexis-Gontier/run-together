58538a93ba179708b3d9d9c0f3cb9493713b8bc4
Gontier <alexis.gontier03@gmail.com>
2025-08-11 23:19:46 +0200

    🔒️ Ajout de la vérification de l'environnement de production pour gérer les erreurs 404 sur la page d'inscription

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -5,8 +5,15 @@ import {
   CardTitle
 } from "@/components/shadcn-ui/card"
 import { SignUpForm } from "./sign-up-form"
+import { env } from "@/utils/env"
+import { notFound } from "next/navigation"
 
 export default function SignUpPage() {
+
+  if (env.NODE_ENV === "production") {
+    notFound()
+  }
+
   return (
     <Card className="w-full">
       <CardContent>
