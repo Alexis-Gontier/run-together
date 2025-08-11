dec67357f171a686bebf29f111b6f54e523b858d
Gontier <alexis.gontier03@gmail.com>
2025-08-11 23:18:10 +0200

    ✨ Mise à jour de la configuration d'authentification pour intégrer la gestion de l'environnement et ajuster les paramètres de session

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,6 +8,7 @@ import {
     admin,
 } from "better-auth/plugins"
 import { nextCookies } from "better-auth/next-js";
+import { env } from "@/utils/env";
 
 import { APP_URL } from "@/utils/constants";
 
@@ -16,19 +17,26 @@ export const auth = betterAuth({
         provider: "postgresql",
     }),
     baseURL: APP_URL,
-    trustedOrigins: [
-        APP_URL
-    ],
+    trustedOrigins: [APP_URL],
     emailAndPassword: {
         enabled: true,
         autoSignIn: false,
         requireEmailVerification: false,
-        // disableSignUp: true,
+        disableSignUp: env.NODE_ENV === "production",
         minPasswordLength: 2,
         // maxPasswordLength: 128,
     },
+    session: {
+        expiresIn: 60 * 60 * 24 * 7, // 7 jours
+        updateAge: 60 * 60 * 24,     // 1 jour
+        cookieCache: {
+            enabled: true,
+            maxAge: 5 * 60 // 5 minutes
+        },
+        freshAge: 60 * 5 // 5 minutes
+    },
     plugins: [
-        openAPI(),
+        ...(env.NODE_ENV === "development" ? [openAPI()] : []),
         username(),
         admin(),
         nextCookies() // make sure this is the last plugin in the array
