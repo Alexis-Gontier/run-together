eea215bd072879e8003852c23db936abb2ee2bf8
Gontier <alexis.gontier03@gmail.com>
2025-08-12 16:03:17 +0200

    ✨ Ajout du composant DashboardLayout avec AppSidebar et DashboardHeader pour une meilleure structure de la page de tableau de bord

---
+++ src/components/layout/dashboard-sidebar.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import { usePathname } from "next/navigation"
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+} from "@/components/shadcn-ui/sidebar"
+import Link from "next/link"
+import { Button } from "@/components/shadcn-ui/button"
+import {
+    Footprints,
+    HomeIcon,
+} from "lucide-react"
+
+const LINKITEMS = [
+    {
+        href: "/dashboard",
+        label: "Home",
+        icon: HomeIcon,
+    },
+    {
+        href: "/run",
+        label: "Run",
+        icon: Footprints,
+    },
+]
+
+export function AppSidebar() {
+    const pathname = usePathname()
+
+    return (
+        <Sidebar>
+            <SidebarContent>
+                <SidebarGroup className="p-4">
+                    {LINKITEMS.map((item) => (
+                        <Button
+                            key={item.href}
+                            className="w-full"
+                            variant={pathname === item.href ? "outline" : "ghost"}
+                            size="lg"
+                            asChild
+                        >
+                            <Link href={item.href} className="flex justify-start">
+                                <item.icon className="size-4" />
+                                {item.label}
+                            </Link>
+                        </Button>
+                    ))}
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
+    )
+}
