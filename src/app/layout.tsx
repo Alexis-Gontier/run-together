abdaa4d6b8daa3118ddfdeb3cfcc118290501874
Gontier <alexis.gontier03@gmail.com>
2025-08-12 02:05:43 +0200

    🔍 Ajout des métadonnées pour le titre de l'application dans le layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 import "@/styles/globals.css";
 import { inter } from "@/styles/fonts";
 
+import type { Metadata } from "next";
+
 import Providers from "@/app/providers";
 import { Toaster } from "@/components/shadcn-ui/sonner"
 import { Analytics } from "@vercel/analytics/next"
+
+export const metadata: Metadata = {
+  title: "RunTogether",
+}
 
 export default function RootLayout({
   children,
