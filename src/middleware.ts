f3bf670b5cfdd4b510551845129d1852f1f96941
Gontier <alexis.gontier03@gmail.com>
2025-08-12 02:05:33 +0200

    🔒️  Ajout de la gestion des chemins publics dans le middleware d'authentification

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
+const publicPaths = [
+    "/",
+];
+
 const authPaths = [
     "/sign-in",
     "/sign-up",
@@ -9,6 +13,10 @@ const authPaths = [
 export async function middleware(request: NextRequest) {
     const sessionCookie = getSessionCookie(request);
     const { pathname } = new URL(request.url);
+
+    if (publicPaths.includes(pathname)) {
+        return NextResponse.next();
+    }
 
     if (sessionCookie && authPaths.includes(pathname)) {
         return NextResponse.redirect(new URL("/dashboard", request.url));
