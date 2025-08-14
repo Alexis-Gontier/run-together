5c998d468c734fc89b785d7a0c9dd106c0d784d1
Gontier <alexis.gontier03@gmail.com>
2025-08-14 22:41:23 +0200

    🔥 Ajout des fonctions secondsToTimeString et timeStringToSeconds pour améliorer la gestion des conversions de temps

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -11,3 +11,15 @@ export function formatSecondsToHMS(totalSeconds: number): string {
 
   return `${seconds}s`
 }
+
+export const secondsToTimeString = (seconds: number): string => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const secs = seconds % 60;
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+};
+
+export const timeStringToSeconds = (timeString: string): number => {
+    const [hours, minutes, seconds] = timeString.split(':').map(Number);
+    return hours * 3600 + minutes * 60 + seconds;
+};
