61ac4362c1f527ebc1f99b9ac68149860abe4d9b
Gontier <alexis.gontier03@gmail.com>
2025-08-14 22:43:03 +0200

    🔥 Réécriture de la page d'édition des courses avec ajout de la logique de récupération des données et amélioration de l'interface utilisateur

--- src/app/dashboard/runs/[id]/edit/page.tsx
+++ src/app/dashboard/runs/[id]/edit/page.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import TextHeading from '@/components/ui/text-heading'
+import { UpdateRunForm } from './edit-form'
+import { Separator } from '@/components/shadcn-ui/separator'
+import { prisma } from "@/lib/prisma"
+import { getUser } from "@/lib/auth-server"
+import { notFound } from "next/navigation"
 
-export default function page() {
+interface PageProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function EditRunPage({ params }: PageProps) {
+  const user = await getUser()
+  if (!user) notFound()
+
+  const { id } = await params;
+  const run = await prisma.run.findFirst({
+    where: {
+      id,
+      userId: user.id,
+    },
+  })
+
+  if (!run) {
+    notFound()
+  }
+
   return (
-    <div>page</div>
+    <div className="max-w-md mx-auto space-y-4">
+      <TextHeading
+        title="Modifier une course"
+        description="Mettez à jour les informations de la course ci-dessous."
+      />
+      <Separator />
+      <UpdateRunForm run={run} />
+    </div>
   )
 }
