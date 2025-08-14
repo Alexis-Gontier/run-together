3e706c64944355852b4f7b523965264565f453f1
Gontier <alexis.gontier03@gmail.com>
2025-08-14 22:42:52 +0200

    🔥 Remplacement du fragment par un conteneur div dans le composant de création de course

--- src/app/dashboard/runs/create/page.tsx
+++ src/app/dashboard/runs/create/page.tsx
@@ -4,13 +4,13 @@ import { Separator } from '@/components/shadcn-ui/separator'
 
 export default function page() {
   return (
-    <>
+    <div className="max-w-md mx-auto space-y-4">
       <TextHeading
         title="Créer une course"
         description="Remplissez le formulaire ci-dessous pour créer une nouvelle course."
       />
       <Separator />
       <CreateRunForm />
-    </>
+    </div>
   )
 }
