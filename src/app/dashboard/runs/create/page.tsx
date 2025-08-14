b4b621a18eb18b6c12a4139e66bc54ae13ade4f6
Gontier <alexis.gontier03@gmail.com>
2025-08-14 07:23:11 +0200

    ✨ Ajout du composant TextHeading et du séparateur dans la page de création de course pour une meilleure structure et clarté

--- src/app/dashboard/runs/create/page.tsx
+++ src/app/dashboard/runs/create/page.tsx
@@ -1,8 +1,15 @@
+import TextHeading from '@/components/ui/text-heading'
 import { CreateRunForm } from './create-form'
+import { Separator } from '@/components/shadcn-ui/separator'
 
 export default function page() {
   return (
     <>
+      <TextHeading
+        title="Créer une course"
+        description="Remplissez le formulaire ci-dessous pour créer une nouvelle course."
+      />
+      <Separator />
       <CreateRunForm />
     </>
   )
