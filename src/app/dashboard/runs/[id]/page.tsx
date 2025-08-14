ca2add0b935fcfffe9267dedbedd21e31f8de261
Gontier <alexis.gontier03@gmail.com>
2025-08-14 07:23:08 +0200

    ✨ Amélioration de la gestion des paramètres dans RunByIdPage pour une meilleure clarté et simplification du code

--- src/app/dashboard/runs/[id]/page.tsx
+++ src/app/dashboard/runs/[id]/page.tsx
@@ -1,20 +1,19 @@
 import { getRunById } from '@/actions/run/get-run-by-id.action';
-import React from 'react'
 
 interface PageProps {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 }
 
 export default async function RunByIdPage({ params }: PageProps) {
 
-  const run = await getRunById(params.id);
-
+  const { id } = await params;
+  const run = await getRunById(id);
 
   return (
     <div>
-      Run Details for {params.id}
+      Run Details for {id}
       <pre>{JSON.stringify(run, null, 2)}</pre>
     </div>
   )
