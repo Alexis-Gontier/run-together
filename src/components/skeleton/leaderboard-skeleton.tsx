2223d86c7c5b2d130cd9561826b5dc16997b5e2d
Gontier <alexis.gontier03@gmail.com>
2025-08-15 02:22:12 +0200

    ✨ Ajout des composants Leaderboard et LeaderboardItem pour afficher le classement des utilisateurs avec leurs distances totales

---
+++ src/components/skeleton/leaderboard-skeleton.tsx
@@ -0,0 +1,7 @@
+import { Skeleton } from "@/components/shadcn-ui/skeleton"
+
+export default function LeaderboardSkeleton() {
+    return (
+        <Skeleton className="w-full h-96 rounded" />
+    )
+}
