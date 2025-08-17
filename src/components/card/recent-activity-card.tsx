2670dc7c3d5031d68705170428ce79124d427256
Gontier <alexis.gontier03@gmail.com>
2025-08-17 07:19:50 +0200

    ✨ Ajout du composant RecentActivityCard pour afficher les dernières activités du groupe

---
+++ src/components/card/recent-activity-card.tsx
@@ -0,0 +1,21 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/shadcn-ui/card"
+
+export default function RecentActivityCard() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Activité Récente</CardTitle>
+        <CardDescription>Les dernières activités de votre groupe</CardDescription>
+      </CardHeader>
+      <CardContent>
+
+      </CardContent>
+    </Card>
+  )
+}
