4af3c8e23e60f995ea9a3f999fa6037f7f9c4dd3
Gontier <alexis.gontier03@gmail.com>
2025-08-17 20:53:59 +0200

    ✨  Réorganisation du composant DashboardPage : ajout du bouton de déconnexion et ajustement de la grille pour une meilleure présentation

--- src/app/dashboard/(home)/page.tsx
+++ src/app/dashboard/(home)/page.tsx
@@ -1,28 +1,21 @@
 import TextHeading from '@/components/ui/text-heading'
 import LeaderboardCard from '@/components/card/leaderboard-card'
 import RecentActivityCard from '@/components/card/recent-activity-card'
+import SignOutButton from '@/components/auth/sign-out-button'
 
 export default function DashboardPage() {
 
   return (
     <>
+      <SignOutButton />
       <TextHeading
         title="Tableau de bord"
         description="Bienvenue sur votre tableau de bord."
       />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         <LeaderboardCard />
         <RecentActivityCard />
       </div>
-      {/* <SignOutButton /> */}
-      {/* <div className="grid grid-cols-2 gap-4"> */}
-        {/* <Suspense fallback={<LeaderboardSkeleton />}>
-          <Leaderboard />
-        </Suspense> */}
-        {/* <Suspense fallback={<LeaderboardSkeleton />}>
-          <RecentActivity />
-        </Suspense> */}
-      {/* </div> */}
     </>
   )
 }
