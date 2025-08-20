02ad88ecfa24167da38f89b68dd05f79ad25538e
Gontier <alexis.gontier03@gmail.com>
2025-08-20 05:30:37 +0200

    ✨ Remplace le texte de chargement par un composant Loader2 stylisé

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,9 @@
+import { Loader2 } from "lucide-react";
+
 export default function Loading() {
   return (
-    <div>loading</div>
-  )
+    <div className="flex items-center justify-center h-screen bg-muted">
+        <Loader2 className="w-12 h-12 animate-spin text-primary" />
+    </div>
+  );
 }
