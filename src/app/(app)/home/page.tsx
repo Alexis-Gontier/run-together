834d4455cf16ba636e17233f2e02c3f446262906
Gontier <alexis.gontier03@gmail.com>
2026-04-21 23:17:26 +0200

    feat(auth): add database env variables to CI/build and implement user auth tables & migration

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -1,7 +1,3 @@
-import { DebugJson } from "@/components/ui/debug-json"
-import { prisma } from "@/lib/db/prisma"
-
 export default async function HomePage() {
-  const data = await prisma.test.findMany()
-  return <DebugJson data={data} />
+  return null
 }
