a8e1c2c9cd8ae83cf228b35545980b8c7b0ce8b6
Gontier <alexis.gontier03@gmail.com>
2026-04-21 03:20:28 +0200

    feat: add countdown component and update dependencies

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,19 +1,10 @@
-"use client"
-
 import { CenteredLayout } from "@/components/layout/centered-layout"
-import { Button } from "@/components/shadcn-ui/button"
-import { toast } from "sonner"
+import { Countdown } from "@/components/ui/countdown"
 
 export default function page() {
-  function handleClick() {
-    toast.success("Hello world!")
-  }
-
   return (
     <CenteredLayout>
-      <Button size="lg" onClick={handleClick} className="cursor-pointer">
-        Click me
-      </Button>
+      <Countdown target="2026-04-26T10:00:00Z" />
     </CenteredLayout>
   )
 }
